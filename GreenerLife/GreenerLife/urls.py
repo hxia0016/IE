59e9cdb19219b9737a6ec977e6653a8f8c8b4b5c
Ziqi Shen <zshe0026@student.monash.edu>
2022-09-17 20:43:54 +1000

    icon and logo

--- GreenerLife/GreenerLife/urls.py
+++ GreenerLife/GreenerLife/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
+from django.conf.urls.static import serve,static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base.urls'))
+    path('', include('base.urls')),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
